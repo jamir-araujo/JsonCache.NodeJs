f26262768c2fc08e66e3a4f65ee977d0dfe62864
Jamir Castro Araújo <jamir.c.a@hotmail.com>
2017-05-31 23:00:53 -0300

    inspecionando arrays

--- src/objectInspector.ts
+++ src/objectInspector.ts
@@ -1,33 +1,39 @@
 import { notNull } from "./check";
 
 export default class ObjectInspector {
-    inspectObject(value: any, cacheItemFounded: Action<any>): void {
+    inspectObject(value: Object, cacheItemFounded: Action<Object>): void {
         notNull(value, "value");
         notNull(cacheItemFounded, "cacheItemFounded");
 
-        this.inspectObjectInternal(value, cacheItemFounded, new HashSet<any>());
+        this.inspectObjectInternal(value, cacheItemFounded, new Set<any>());
     }
 
-    private inspectObjectInternal(value: any, cacheItemFounded: Action<any>, workingItems: HashSet<any>): void {
+    private inspectObjectInternal(value: any, cacheItemFounded: Action<any>, workingItems: Set<any>): void {
         if (value === null || !workingItems.add(value)) {
             return;
         }
 
         if (value instanceof Array) {
-
+            for (let item of value) {
+                this.inspectObjectInternal(item, cacheItemFounded, workingItems);
+            }
         } else if (value instanceof Object) {
             cacheItemFounded(value);
 
-            for (var key in value) {
-                if (value.hasOwnProperty(key)) {
-                    this.inspectObjectInternal(value[key], cacheItemFounded, workingItems);
-                }
+            this.inspectObjectProperties(value, cacheItemFounded, workingItems);
+        }
+    }
+
+    private inspectObjectProperties(value: any, cacheItemFounded: Action<any>, workingItems: Set<any>) {
+        for (var key in value) {
+            if (value.hasOwnProperty(key)) {
+                this.inspectObjectInternal(value[key], cacheItemFounded, workingItems);
             }
         }
     }
 }
 
-class HashSet<T> {
+class Set<T> {
     private _items: T[] = [];
 
     add(item: T): boolean {
--- src/tests/objectInspector.test.ts
+++ src/tests/objectInspector.test.ts
@@ -1,4 +1,4 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import ObjectInspector from "../objectInspector";
 
 describe("ObjectInspector", () => {
@@ -9,6 +9,19 @@ describe("ObjectInspector", () => {
     });
 
     describe("inspectObject", () => {
+
+        // it("should throw when value is null", () => {
+        //     expect(() => objectInspector.inspectObject(null, () => { }))
+        //         .to
+        //         .throw("parameter value can not be null", "should have thrown an exception");
+        // });
+
+        // it("should throw when cacheItemFounded is null", () => {
+        //     var nullAction: Action<object> | null = null;
+        //     expect(() => objectInspector.inspectObject({}, nullAction));
+        //         .to
+        //         .throw("parameter cacheItemFounded can not be null", "should have thrown an exception")
+        // });
 
         it("should call cacheItemFounded only once for shallow objects", () => {
             var shallowObject = { value: 1 };
@@ -30,9 +43,49 @@ describe("ObjectInspector", () => {
             });
 
             assert.equal(2, objects.length, "callCount should be 2");
-
             assert.equal(objects[0], complexObject, "objects[0] and complexObject should the same");
             assert.equal(objects[1], complexObject.nested, "objects[1] and complexObject.nested should be same");
+        });
+
+        it("should not call cacheItemFounded for property when value is null", () => {
+            var complexObject = { value: 1, nested: null };
+            var objects: any[] = [];
+
+            objectInspector.inspectObject(complexObject, value => {
+                objects.push(value);
+            });
+
+            assert.equal(1, objects.length, "callCount should be 1");
+            assert.equal(objects[0], complexObject, "objects[0] and complexObject should the same");
+        });
+
+        it("should call for each individual item in the nested array", () => {
+            var complexObject = { value: 1, nestedArray: [{}, {}] };
+
+            var objects: any[] = [];
+
+            objectInspector.inspectObject(complexObject, value => {
+                objects.push(value);
+            });
+
+            assert.equal(3, objects.length, "callCount should be 3");
+            assert.equal(objects[0], complexObject, "objects[0] and complexObject should the same");
+            assert.equal(objects[1], complexObject.nestedArray[0], "objects[1] and complexObject.nestedArray[0] should be the same");
+            assert.equal(objects[2], complexObject.nestedArray[1], "objects[1] and complexObject.nestedArray[1] should be the same");
+        });
+
+        it("should not call cacheItemFounded for null item in the nested array", () => {
+            var complexObject = { value: 1, nestedArray: [{}, null] };
+
+            var objects: any[] = [];
+
+            objectInspector.inspectObject(complexObject, value => {
+                objects.push(value);
+            });
+
+            assert.equal(2, objects.length, "callCount should be 2");
+            assert.equal(objects[0], complexObject, "objects[0] and complexObject should the same");
+            assert.equal(objects[1], complexObject.nestedArray[0], "objects[1] and complexObject.nestedArray[0] should be the same");
         });
 
     });
